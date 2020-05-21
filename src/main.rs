2e3f2e87d2e613e5bddf1855abd83a0a1ab6617d
John Murphy <murphybytes@gmail.com>
2020-05-21 11:42:49 -0500

    add command line capability

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+extern crate clap;
+use clap::{App, Arg};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("udp-copy")
+                    .arg(Arg::with_name("send")
+                        )
+                    .arg(Arg::with_name("server")
+                            .takes_value(false)
+                        )
+                    .version("0.0.1")
+                    .about("fast copy big files")
+                    .get_matches();
+
+    if matches.is_present("server") {
+        println!("server set");
+    }               
+
 }
